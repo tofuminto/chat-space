83fdb7a435f781ab1637270ab8d89cda4eb584be
tofuminto <tototofumifumin@gmail.com>
2020-06-24 22:59:08 +0900

    automatic_updating

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message) {
     if (message.image){
       var html = 
-        `<div class="messages">
+        `<div class="messages" data-message-id=${message.id}>
           <div class="message__user">
             <div class="message__user--name">
               ${message.user_name}
@@ -19,7 +19,7 @@ $(function(){
       return html;
     } else {
       var html = 
-        `<div class="messages">
+        `<div class="messages" data-message-id=${message.id}>
           <div class="message__user">
             <div class="message__user--name">
               ${message.user_name}
@@ -61,4 +61,29 @@ $(function(){
       $('.submit__btn').prop('disabled', false);
     });
   });
+  var reloadMessages = function() {
+    var last_message_id = $('.messages:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.chat__main__message--list').append(insertHTML);
+        $('.chat__main__message--list').animate({ scrollTop: $('.chat__main__message--list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
